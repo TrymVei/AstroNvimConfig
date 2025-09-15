12b59a13272ef5cc70c9f81e407780b35c874816
TrymVei <tve@variant.no>
2025-09-15 12:48:55 +0200

    feat: change keybinding for copilot autocomplete

--- lua/configs/copilot.lua
+++ lua/configs/copilot.lua
@@ -21,7 +21,7 @@ require("copilot").setup {
     hide_during_completion = true,
     debounce = 75,
     keymap = {
-      accept = "<M-l>",
+      accept = "<C-l>",
       accept_word = false,
       accept_line = false,
       -- next = "<M-]>",
